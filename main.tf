e52ffb8191f8b019f428f40ddeb9a3936181d4c6
aniljeenapati <jeenapati.anil@gmail.com>
2024-10-01 15:22:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,11 @@
 provider "google" {
-  project = "primal-gear-436812-t0"
+  project = "primal-gear-436812-t0"  # Update with your GCP project ID
   region  = "us-central1"
 }
 
-resource "google_compute_instance" "centos_vm" {
-  name         = "centosvm1"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "centos-cloud/centos-stream-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
-
-  metadata = {
-    ssh-keys = "ansible:${file("/root/.ssh/id_rsa.pub")}"
-  }
-  
-  tags = ["http-server"]
-  provisioner "local-exec" {
-    command = <<EOF
-      mkdir -p ansible
-      echo "[webserver]" > ansible/inventory
-      echo "${self.network_interface.0.access_config.0.nat_ip} ansible_user=ansible ansible_ssh_private_key_file=/root/.ssh/id_rsa" >> ansible/inventory
-    EOF
-  }
-}
+# Creating a firewall rule to allow HTTP traffic
 resource "google_compute_firewall" "http-server" {
-  name    = "http-server-firewall"
+  name    = "http-server"
   network = "default"
 
   allow {
@@ -42,9 +13,41 @@ resource "google_compute_firewall" "http-server" {
     ports    = ["80"]
   }
 
+  # Allow traffic from everywhere to instances with an HTTP server tag
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server"]
 }
+
+# Creating the CentOS VM instance
+resource "google_compute_instance" "centos_vm" {
+  name         = "centos-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-9"  # Correct image family
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  # Specify your SSH key for accessing the VM
+  metadata = {
+    ssh-keys = "centos:${file("/root/.ssh/id_rsa.pub")}"  # Update this path to your public key
+  }
+
+  tags = ["http-server"]
+
+  # Using a provisioner to write the VM's public IP to the Ansible inventory file
+  provisioner "local-exec" {
+    command = "mkdir -p ansible && echo ${self.network_interface.0.access_config.0.nat_ip} > ansible/inventory"
+  }
+}
+
 output "vm_ip" {
   value = google_compute_instance.centos_vm.network_interface.0.access_config.0.nat_ip
 }
