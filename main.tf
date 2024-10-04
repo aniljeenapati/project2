cc6b34c6f7734cf63ba8fb53a65f906e5bdc724f
aniljeenapati <jeenapati.anil@gmail.com>
2024-10-04 19:31:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,13 +82,13 @@ output "lb_external_ip" {
 resource "null_resource" "update_inventory" {
   provisioner "local-exec" {
     command = <<EOT
-      INSTANCE_IDS=$(gcloud compute instance-groups list-instances apache-instance-group --zone us-central1-a --format="value(instance)")
+      INSTANCE_IDS=\$(gcloud compute instance-groups list-instances apache-instance-group --zone us-central1-a --format="value(instance)")
       echo 'all:' > /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
       echo '  hosts:' >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-      for INSTANCE_ID in $INSTANCE_IDS; do
-        INSTANCE_IP=$(gcloud compute instances describe $INSTANCE_ID --zone us-central1-a --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
-        echo "    web_${INSTANCE_ID}:" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-        echo "      ansible_host: \${INSTANCE_IP}" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
+      for INSTANCE_ID in \$INSTANCE_IDS; do
+        INSTANCE_IP=\$(gcloud compute instances describe \$INSTANCE_ID --zone us-central1-a --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
+        echo "    web_\$INSTANCE_ID:" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
+        echo "      ansible_host: \$INSTANCE_IP" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
         echo "      ansible_user: centos" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
         echo "      ansible_ssh_private_key_file: /root/.ssh/id_rsa" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
       done
@@ -97,3 +97,4 @@ resource "null_resource" "update_inventory" {
 
   depends_on = [google_compute_instance_group_manager.default]
 }
+
