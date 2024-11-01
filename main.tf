6782ba8722cf6e83ff81050cc8d64ce4cecf1823
aniljeenapati <jeenapati.anil@gmail.com>
2024-11-01 15:32:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,34 +39,17 @@ resource "google_compute_instance_group_manager" "default" {
   }
 }
 
-output "vm_ips" {
-  value = [for instance in data.google_compute_instance.instances : instance.network_interface[0].access_config[0].nat_ip]
+data "google_compute_instance_group" "default" {
+  name = google_compute_instance_group_manager.default.instance_group
+  zone = "us-central1-a"
 }
 
-# Create a null resource to fetch the IPs after the instances are created
-resource "null_resource" "get_instance_ips" {
-  provisioner "local-exec" {
-    command = <<EOT
-      # Get the instance group instances and their IPs
-      gcloud compute instance-groups list-instances ${google_compute_instance_group_manager.default.instance_group} \
-      --zone ${google_compute_instance_group_manager.default.zone} \
-      --format="get(instance,networkInterfaces[0].accessConfigs[0].natIP)" > instance_ips.txt
+data "google_compute_instance" "instances" {
+  count = length(data.google_compute_instance_group.default.instances)
+  name  = data.google_compute_instance_group.default.instances[count.index]
+  zone  = "us-central1-a"
+}
 
-      # Generate the Ansible inventory from the instance IPs
-      echo "all:" > inventory.gcp.yml
-      echo "  children:" >> inventory.gcp.yml
-      echo "    web:" >> inventory.gcp.yml
-      echo "      hosts:" >> inventory.gcp.yml
-      while read ip; do
-        echo "        web_ansible-\$ip:" >> inventory.gcp.yml
-        echo "          ansible_host: \$ip" >> inventory.gcp.yml
-        echo "          ansible_user: centos" >> inventory.gcp.yml
-        echo "          ansible_ssh_private_key_file: /var/lib/jenkins/.ssh/id_rsa" >> inventory.gcp.yml
-      done < instance_ips.txt
-    EOT
-  }
-
-  depends_on = [
-    google_compute_instance_group_manager.default
-  ]
+output "vm_ips" {
+  value = [for instance in data.google_compute_instance.instances : instance.network_interface[0].access_config[0].nat_ip]
 }
