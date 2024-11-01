05f94848b110a6ba49c56827fc54585b75401ee8
aniljeenapati <jeenapati.anil@gmail.com>
2024-11-01 17:21:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ provider "google" {
 resource "google_compute_instance_template" "default" {
   name           = "apache-instance-template"
   machine_type   = "e2-medium"
-  region         = "us-central1"
-
+  
   disk {
     auto_delete  = true
     boot         = true
@@ -20,21 +19,14 @@ resource "google_compute_instance_template" "default" {
 
   metadata = {
     ssh-keys = "centos:${file("/var/lib/jenkins/.ssh/id_rsa.pub")}"
-    }
+  }
 
   metadata_startup_script = <<-EOF
     #!/bin/bash
     sudo systemctl start sshd
   EOF
-  metadata = {
-    ssh-keys = "centos:${file("/var/lib/jenkins/.ssh/id_rsa.pub")}"
-  }
 
   tags = ["http-server"]
-}
-
-output "vm_ips" {
-  value = [for instance in google_compute_instance.centos_vm : instance.network_interface[0].access_config[0].nat_ip]
 }
 
 resource "null_resource" "generate_inventory" {
@@ -44,7 +36,7 @@ resource "null_resource" "generate_inventory" {
       echo '  children:' >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
       echo '    web:' >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
       echo '      hosts:' >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
-      for i in $(seq 0 2); do
+      for i in $(seq 0 $((${google_compute_instance_group_manager.default.target_size} - 1))); do
         INSTANCE_IP=$(terraform output -json vm_ips | jq -r ".[$i]")
         echo "        web_ansible-$((i + 1)):" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
         echo "          ansible_host: \$INSTANCE_IP" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
@@ -52,10 +44,8 @@ resource "null_resource" "generate_inventory" {
         echo "          ansible_ssh_private_key_file: /var/lib/jenkins/.ssh/id_rsa" >> /var/lib/jenkins/workspace/loadbalancer/inventory.gcp.yml
       done
     EOT
-   }
- }
+  }
 }
-
 
 resource "google_compute_instance_group_manager" "default" {
   name               = "apache-instance-group"
