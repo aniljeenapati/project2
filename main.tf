e05b7ead419d9990515142689bf3592aa8adfb09
aniljeenapati <jeenapati.anil@gmail.com>
2024-11-01 15:26:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,40 +39,26 @@ resource "google_compute_instance_group_manager" "default" {
   }
 }
 
-# Data block to get the instance group instances
-data "google_compute_instance_group" "default" {
-  name = google_compute_instance_group_manager.default.instance_group
-  zone = "us-central1-a"
-}
-
-# Fetching the instances separately without using count
-data "google_compute_instance" "instances" {
-  for_each = toset(data.google_compute_instance_group.default.instances)
-
-  name = each.key
-  zone = "us-central1-a"
-}
-
-# Output the VM IPs
-output "vm_ips" {
-  value = [for instance in data.google_compute_instance.instances : instance.network_interface[0].access_config[0].nat_ip]
-}
-
-# Resource to generate Ansible inventory
-resource "null_resource" "generate_ansible_inventory" {
+# Create a null resource to fetch the IPs after the instances are created
+resource "null_resource" "get_instance_ips" {
   provisioner "local-exec" {
     command = <<EOT
-      # Get the VM IPs and generate Ansible inventory
+      # Get the instance group instances and their IPs
+      gcloud compute instance-groups list-instances ${google_compute_instance_group_manager.default.instance_group} \
+      --zone ${google_compute_instance_group_manager.default.zone} \
+      --format="get(instance,networkInterfaces[0].accessConfigs[0].natIP)" > instance_ips.txt
+
+      # Generate the Ansible inventory from the instance IPs
       echo "all:" > inventory.gcp.yml
       echo "  children:" >> inventory.gcp.yml
       echo "    web:" >> inventory.gcp.yml
       echo "      hosts:" >> inventory.gcp.yml
-      for ip in \$(terraform output -json vm_ips | jq -r '.[]'); do
+      while read ip; do
         echo "        web_ansible-\$ip:" >> inventory.gcp.yml
         echo "          ansible_host: \$ip" >> inventory.gcp.yml
         echo "          ansible_user: centos" >> inventory.gcp.yml
         echo "          ansible_ssh_private_key_file: /var/lib/jenkins/.ssh/id_rsa" >> inventory.gcp.yml
-      done
+      done < instance_ips.txt
     EOT
   }
 
