2dbef4b37a2cc726a371b805c8cb5b064c3ff1e4
aniljeenapati <jeenapati.anil@gmail.com>
2024-11-01 13:43:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,85 @@
 provider "google" {
   project = "primal-gear-436812-t0"
-  region  = "us-central1"
 }
 
-variable "instance_count" {
-  default = 2
-}
+resource "google_compute_instance_template" "default" {
+  name           = "apache-instance-template"
+  machine_type   = "e2-medium"
+  region         = "us-central1"
 
-resource "google_compute_instance" "apache_instance" {
-  count        = var.instance_count
-  name         = "apache-instance-${count.index}"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-  tags         = ["http-server"]
-
-  boot_disk {
-    initialize_params {
-      image = "centos-cloud/centos-stream-9"
-    }
+  disk {
+    auto_delete  = true
+    boot         = true
+    source_image = "centos-cloud/centos-stream-9"
   }
 
   network_interface {
     network = "default"
     access_config {}
   }
-
   metadata_startup_script = <<-EOF
     #!/bin/bash
     sudo systemctl start sshd
-  EOF
+    EOF
+}
+
+resource "google_compute_instance_group_manager" "default" {
+  name               = "apache-instance-group"
+  version {
+    instance_template = google_compute_instance_template.default.id
+  }
+  base_instance_name = "apache-instance"
+  target_size        = 2
+  zone               = "us-central1-a"
+
+  named_port {
+    name = "http"
+    port = 80
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name          = "apache-backend-service"
+  backend {
+    group = google_compute_instance_group_manager.default.instance_group
+  }
+  health_checks = [google_compute_http_health_check.default.id]
+  port_name     = "http"
+  protocol      = "HTTP"
+  timeout_sec   = 30
+  load_balancing_scheme = "EXTERNAL"
+}
+
+resource "google_compute_http_health_check" "default" {
+  name                    = "apache-health-check"
+  request_path            = "/"
+  port                    = 80
+  check_interval_sec      = 20
+  timeout_sec             = 15
+  healthy_threshold       = 1
+  unhealthy_threshold     = 3
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "apache-url-map"
+  default_service = google_compute_backend_service.default.id
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name    = "apache-http-proxy"
+  url_map = google_compute_url_map.default.id
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "apache-forwarding-rule"
+  target     = google_compute_target_http_proxy.default.id
+  port_range = "80"
+}
+
+data "google_compute_instance_group" "default" {
+  # Reference the instance group created by the instance group manager
+  # Use the instance_group attribute from the instance group manager
+  self_link = google_compute_instance_group_manager.default.instance_group
 }
 
 output "vm_ips" {
