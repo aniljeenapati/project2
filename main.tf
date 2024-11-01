9110453d00c14b6d6e526ad353aa13e582fd0934
aniljeenapati <jeenapati.anil@gmail.com>
2024-11-01 14:00:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,13 +17,11 @@ resource "google_compute_instance_template" "default" {
     network = "default"
     access_config {}
   }
+
   metadata_startup_script = <<-EOF
     #!/bin/bash
     sudo systemctl start sshd
-    EOF
-}
-output "vm_ips" {
-  value = [for instance in google_compute_instance.apache_instance : instance.network_interface[0].access_config[0].nat_ip]
+  EOF
 }
 
 resource "google_compute_instance_group_manager" "default" {
@@ -39,6 +37,46 @@ resource "google_compute_instance_group_manager" "default" {
     name = "http"
     port = 80
   }
+}
+
+# Data block to get the instance group instances
+data "google_compute_instance_group" "default" {
+  instance_group = google_compute_instance_group_manager.default.id
+  zone           = "us-central1-a"
+}
+
+data "google_compute_instance" "instances" {
+  count        = length(data.google_compute_instance_group.default.instances)
+  name         = data.google_compute_instance_group.default.instances[count.index].name
+  zone         = "us-central1-a"
+}
+
+# Output the VM IPs
+output "vm_ips" {
+  value = [for instance in data.google_compute_instance.instances : instance.network_interface[0].access_config[0].nat_ip]
+}
+
+# Resource to generate Ansible inventory
+resource "null_resource" "generate_ansible_inventory" {
+  provisioner "local-exec" {
+    command = <<EOT
+      # Get the VM IPs and generate Ansible inventory
+      echo "all:" > inventory.gcp.yml
+      echo "  children:" >> inventory.gcp.yml
+      echo "    web:" >> inventory.gcp.yml
+      echo "      hosts:" >> inventory.gcp.yml
+      for ip in $(terraform output -json vm_ips | jq -r '.[]'); do
+        echo "        web_ansible-${ip}:" >> inventory.gcp.yml
+        echo "          ansible_host: ${ip}" >> inventory.gcp.yml
+        echo "          ansible_user: centos" >> inventory.gcp.yml
+        echo "          ansible_ssh_private_key_file: /var/lib/jenkins/.ssh/id_rsa" >> inventory.gcp.yml
+      done
+    EOT
+  }
+
+  depends_on = [
+    google_compute_instance_group_manager.default
+  ]
 }
 
 resource "google_compute_backend_service" "default" {
